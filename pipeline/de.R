22616b87d546490e091564399203334cd9758195
Daniel Cameron <cameron.d@wehi.edu.au>
2016-07-12 15:15:28 +1000

    Added aggregate counts

--- pipeline/de.R
+++ pipeline/de.R
@@ -1,7 +1,17 @@
+#biocLite("edgeR", "locfit")
 require(edgeR)
 
 # edgeR Quick Start
-fcounts <- read.delim("featureCounts.out.txt")
+utrcounts <- read.delim("featureCounts.out.txt")
+cdscounts <- read.delim("featureCounts.CDS.out.txt")
+
+utrx <- utrcounts[names(utrcounts)[-(1:6)]]
+cdsx <- cdscounts[names(cdscounts)[-(1:6)]]
+
+overallcounts <- colSums(utrx) / colSums(cdsx)
+
+fcounts <- utrcounts
+
 x <- fcounts[names(fcounts)[-(1:6)]]
 rownames(x) <- paste0(fcounts$Geneid, "__", seq_along(fcounts$Geneid))
 group <- factor(c(1,1,1,1,1,2,2,2,2,2)) # ax=1 bod=2
@@ -9,7 +19,6 @@ y <- DGEList(counts=x,group=group)
 y <- calcNormFactors(y)
 design <- model.matrix(~group)
 y <- estimateDisp(y,design)
-
 fit <- glmQLFit(y,design)
 qlf <- glmQLFTest(fit,coef=2)
 topTags(qlf)
